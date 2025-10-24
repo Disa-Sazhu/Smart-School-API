f58e770b31643cb2f260bf4b50472cd98de21b2c
SuthaMar <maryloodsikamaths@.com>
2025-10-24 17:14:31 +0530

    new hello

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Smart_School_Managment
         {
 
             //hi
+            //hello
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
